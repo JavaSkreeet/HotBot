d8549bed0f364e049d92019c4caa6ac2d43ba799
Prateek Nayak <41796509+RagingParakeet@users.noreply.github.com>
2018-07-27 21:54:16 +0530

    Create template.js

---
+++ commands/template.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot,message,args) => {
+
+    //add your code here for the specific command
+    
+}
+
+module.exports.help = {
+
+    name:"" //add name of the command here
+    
+    //make sure name of the command is same as the name of the file
+    //for example ban command should have name "ban" and should be saved as ban.js
+}
