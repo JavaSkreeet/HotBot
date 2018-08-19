b93ccd70a78b049753723fd824c20c79e4091b3a
Prateek <41796509+flowkraD@users.noreply.github.com>
2018-08-19 22:28:56 +0530

    Update headpat.js
    Improved the readability of headpat.js

--- commands/headpat.js
+++ commands/headpat.js
@@ -1,19 +1,26 @@
-const Discord = require("discord.js");
-const randomPuppy = require('random-puppy');//random-puppy API to fetch URL of random images from particulkar subreddit
+const Discord = require("discord.js");      //Discord Client
+const randomPuppy = require('random-puppy');//random-puppy API to fetch URL of random images from particular subreddit
 
-module.exports.run = async (bot,message,args) => {
+module.exports.run = async (bot,message,args) => {  
+    //Getting image URL from subreddit /r/headpats
     randomPuppy('headpats')
     .then(url => {
+        //Finding channel headpat
         let hpchan = message.guild.channels.find(`name`,"headpat");
         if(!hpchan) return message.channel.send("Couldn't find #headpat");
 
         //give bot admin privelages for this to work
+        //Deleting the message and printing the image URL
         message.delete().catch(O_o=>{});
         hpchan.send(`${url}`);
         return;
+        
+        //Due to integration with imgur.com in Discord an imgur image URL will show the
+        //photo preview in the chat and hence saves the hassle of downloading and reuploading
     })
 }
 
+//Properties
 module.exports.help = {
     name:"headpat"
 }
