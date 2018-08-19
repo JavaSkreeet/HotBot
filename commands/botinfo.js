220890dd5d886761101a0dab2309ef973fdaa293
Prateek <41796509+flowkraD@users.noreply.github.com>
2018-08-19 22:24:44 +0530

    Update botinfo.js
    Improved Readability of botinfo.js

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -1,19 +1,24 @@
-const Discord = require("discord.js");
+const Discord = require("discord.js");  //Discord client
 
 module.exports.run = async (bot,message,args) => {
-    let bicon= bot.user.displayAvatarURL;//extracts vot img as url
+    
+    //Extract's Bot's image as URL
+    let bicon= bot.user.displayAvatarURL;
+    //Embedded message of botinfo
     let botembed = new Discord.RichEmbed()
     .setDescription("Bot Information")
     .setColor("#c6002e")
     .setThumbnail(bicon)
     .addField("Bot Name",bot.user.username)
     .addField("Created On",bot.user.createdAt);
-
+    
+    //deleting the request
     message.delete().catch(O_o=>{});
-
+    //printing the bot info
     return message.channel.send(botembed);
 }
 
+//Properties
 module.exports.help = {
     name:"botinfo"
 }
