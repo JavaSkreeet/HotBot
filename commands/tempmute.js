868001f99566fe787d32e5b8a616452eb39ed98a
Prateek <41796509+flowkraD@users.noreply.github.com>
2018-08-19 22:46:58 +0530

    Update tempmute.js
    Improved readability of tempmute.js

--- commands/tempmute.js
+++ commands/tempmute.js
@@ -1,14 +1,21 @@
-const Discord = require("discord.js");
-const ms = require("ms");
+const Discord = require("discord.js");  //Discord Client
+const ms = require("ms");               //Keeps a track of time for mute
 
 module.exports.run = async (bot,message,args) => {
+    
+    //Finding user to be muted
     let toMute=message.guild.member(message.mentions.users.first() || message.guild.member.get(args[0]));
     if(!toMute) return message.channel.send("Couldn't find user");
-    if(toMute.hasPermission("MANAGE_CHANNELS"))
-        return message.reply("Kiso Chup Bethne Ko Bol Raha Hai?");
     
-    let muteRoll=message.guild.role.find(`name`,"muted");
+    //Checking permission
+    if(toMute.hasPermission("MANAGE_CHANNELS"))
+        return message.reply("You don't have the authority to take this action");
+    
     //creating a mute role
+    let muteRoll=message.guild.role.find(`name`,"muted");
+    
+    //checking if mute role exists
+    //If not creating it
     if(!muteRoll){
         try{
             muteRoll = await message.guild.createRole({
@@ -25,13 +32,15 @@ module.exports.run = async (bot,message,args) => {
         }catch(e){
             console.log(e.stack);
         }
-    }
-
+    }//Adding all the necessary restrictions
+    
+    //finding mute time
     let muteTime = args[1];
     if(!muteTime) return message.reply("You didn't specify the time");
 
     await(toMute.addRole(muteRoll.id));
-
+    
+    //Creatinf an embed to display
     let muteembed = new Discord.RichEmbed()
     .setDescription("Mute")
     .setColor("#c6002e")
@@ -41,21 +50,25 @@ module.exports.run = async (bot,message,args) => {
     .addField("Time",`${ms(ms(muteTime))}`);
     return;
 
+    //finding channel #incidents
     let mchan = message.guild.channels.find(`name`,"incidents");
     if(!mchan) return message.channel.send("Couldn't find #incidents");
 
     //give bot admin privelages for this to work
+    //Sending the report
     message.delete().catch(O_o=>{});
     kchan.send(muteembed);
 
     message.reply(`<@${toMute.id}> has been muted for ${ms(ms(muteTime))}`);
-
+    
+    //timeout to lift mute role after the specific time
     setTimeout(function(){
          toMute.removeRole(muteRoll.id);
          message.channel.send(`<@${toMute.id}> has been unmuted`);
     },ms(muteTime));
 }
 
+//Properties
 module.exports.help = {
     name:"tempmute"
 }
