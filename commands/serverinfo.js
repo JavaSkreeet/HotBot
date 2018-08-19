8ae927c8fef5953767d125de010789f41d6485ea
Prateek <41796509+flowkraD@users.noreply.github.com>
2018-08-19 22:41:51 +0530

    Update serverinfo.js
    Improving readability of serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -1,7 +1,11 @@
-const Discord = require("discord.js");
+const Discord = require("discord.js");  //Discord Client
 
 module.exports.run = async (bot,message,args) => {
+    
+    //Getting the server icon as URL
     let sicon = message.guild.iconURL;
+    
+    //Embeded message with server info
     let serverembed=new Discord.RichEmbed()
     .setDescription("Server Information")
     .setColor("#c6002e")
@@ -10,10 +14,12 @@ module.exports.run = async (bot,message,args) => {
     .addField("Created On",message.guild.createdAt)
     .addField("You Joined",message.member.joinedAt)
     .addField("Total Members",message.guild.memberCount);
-
+    
+    //Returning the server info to chat
     return message.channel.send(serverembed);
 }
 
+//Properties
 module.exports.help = {
     name:"serverinfo"
 }
