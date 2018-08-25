5cfabe51640974f8a9a597a505465a34032cb885
Prateek <41796509+flowkraD@users.noreply.github.com>
2018-08-25 12:12:18 +0530

    Create express.js
    Added express.js that keeps the bot alive

---
+++ express.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  console.log(Date.now() + " Ping Received");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+
+/*
+Glitch disables the bot after 5 minutes of inactivity
+We get around this by using the pre installed express module
+and pinging the sirver every 5 minutes
+*/
