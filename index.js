2e39ffdfd945fbea90a06420b9fc00e08e42ce90
Prateek <41796509+flowkraD@users.noreply.github.com>
2018-09-10 21:21:27 +0530

    Added !splay from dev branch

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const botconf = require("./botconfig.json");//Consists of the prefix and bot token
 const Discord = require("discord.js");      //Discord Library
 const fs = require("fs");                   //File System to access the bot commands
-const ytdl = require("ytdl-core");          //To fetch music stream from Youtube
+const YTDL = require("ytdl-core");          //To fetch music stream from Youtube
 const ytsr = require("ytsr");               //YTSR is the library that fetches Youtube URL request based on search queries
 
 const bot = new Discord.Client();           //Creating a bot client
@@ -37,15 +37,13 @@ var servers = {};//Server Queue
 function play(connection,message){
     var server = servers[message.guild.id];//finding the right server request
     //Dispaching the audio stream to voice channel
-    server.dispacher = connection.playStream(ytdl(server.queue[0],{filter: "audioonly",quality:"highestaudio"}));
+    server.dispacher = connection.playStream(YTDL(server.queue[0],{filter: "audioonly",quality:"highestaudio"}));
     server.queue.shift();//Removing the current URL
     
     //Checking if there are any more songs to play else end dispaching
     server.dispacher.on("end",function(){
     if(server.queue[0]) play(connection,message);
     else connection.disconnect();
-                message.channel.send("You must be in  a voice channel (^_^)");
-            
     });
 }
 
@@ -56,64 +54,21 @@ bot.on("message",async message =>{
     if(message.author.bot)return;
     if(message.channel.type === "dm") return;
     
-    if(message.content.includes("https://discord.gg/")||message.content.includes("https://discordapp.com/invite/"){
-        message.delete().catch(O_o=>{});//Deleting Spam
-        message.channel.send("You can't put contents like that here without permission");
-        return;
-        
-    }
-    
     //taking the message and splitting it as command and parameter
     let prefix = botconf.prefix;
     let messageArr = message.content.split(" ");
     let cmd = messageArr[0];
-    let args = messageArr.slice(1);   
+    let args = messageArr.slice(1);
+    let sflag = flase;
     // Above splits the commands as !<cmd> <args>
     
     //Checking for !yt command
     if(cmd === `${prefix}yt` && message.channel.name === "music" && message.channel.type === "text"){
         //Getting command and video URL
         let cmds = args[0];
-        let arg = args[1];
-    
-        if(cmds === "play"){
-            //Play Command
-            //Checking if URL exists
-            if(!arg){
-                message.channel.send("Please provide the video link (^_^)");
-                return;
-            }
-            //Checking for a valid URL
-            if(!ytdl.validateURL(arg)){
-                message.channel.send("Please provide a valid video link (^_^)");
-                return;
-            }
-            //Checking if user is in an audio channel
-            if(!message.member.voiceChannel){
-                message.channel.send("You must be in  a voice channel (^_^)");
-                return;   
-            }
-            
-            //Adiidng queue for individual channel
-            if(!servers[message.guild.id])
-                servers[message.guild.id] = {
-                    queue: []
-                }
-            
-            //Pushing request into the queue
-            var server = servers[message.guild.id];
-            server.queue.push(arg);
-            message.delete().catch(O_o=>{});//Deleting Requests
-        
-            //Join the same audio channel to play the music
-            if(!message.guild.voiceConnection){
-                message.member.voiceChannel.join()
-                .then(function(connection){
-                    play(connection,message);
-                });
-            }      
-        }
-        else if(cmds === "skip"){
+        let arg = args[1];    
+
+        if(cmds === "skip"){
             //Skip command
             var server = servers[message.guild.id];
             message.delete().catch(O_o=>{});
@@ -159,6 +114,90 @@ bot.on("message",async message =>{
                     });
                 });
             });
+        }
+        else if(cmds == "splay"){
+            let filter;//search result
+            
+            //Checking for existance of sarch query
+            if(!arg){
+                message.channel.send("I can't search nothing. Please specify the search field (^_^)");
+                return;
+            }
+            
+            //Getting URL for the given search query
+            ytsr.getFilters(arg, function(err, filters) {
+                if(err) throw err;
+                
+                //Getting a video
+                filter = filters.get("Type").find(o => o.name === "Video");
+                ytsr.getFilters(filter.ref, function(err, filters) {
+                    if(err) throw err;
+                    //Filtering the content based on length to stop trolls
+                    filter = filters.get("Duration").find(o => o.name.startsWith("Short"));
+                    var options = {
+                        limit: 2,               //2 because 1 is too mainstream
+                        nextpageRef: filter.ref //Getting next page reference
+                    }
+                    
+                    //fetching theURL
+                    ytsr(null, options, function(err, searchResults) {
+                        if(err) throw err;
+                        
+                        /*
+                        Due to lack of goto statements if not my lack of knowledge about them
+                        I made this "not so efficient implimentation" by making a seperate if
+                        for play function away from the if else if ladder and the following code 
+                        will change the arg to a I'm feeling lucky URL for a youtube video and
+                        change the cmds to "play" so that it enters the following if statement.
+                        Till i find a better way i think this is the way it's going down.
+                        */
+                        arg = searchResults.items[0].link;
+                        cmds = "play";
+                        sflag = true;
+                    });
+                });
+            });
+        }
+
+        if(cmds === "play"){
+            //Play Command
+            //Checking whether valid URL exists for the user input else skipping for splay generated URL
+            if(!sflag){
+                if(!arg){
+                    message.channel.send("Please provide the video link (^_^)");
+                    return;
+                }
+                //Checking for a valid URL
+                if(!ytdl.validateURL(arg)){
+                    message.channel.send("Please provide a valid video link (^_^)");
+                    return;
+                }
+            }
+
+            //Checking if user is in an audio channel
+            if(!message.member.voiceChannel){
+                message.channel.send("You must be in  a voice channel (^_^)");
+                return;   
+            }
+            
+            //Adiidng queue for individual channel
+            if(!servers[message.guild.id])
+                servers[message.guild.id] = {
+                    queue: []
+                }
+            
+            //Pushing request into the queue
+            var server = servers[message.guild.id];
+            server.queue.push(arg);
+            message.delete().catch(O_o=>{});//Deleting Requests
+        
+            //Join the same audio channel to play the music
+            if(!message.guild.voiceConnection){
+                message.member.voiceChannel.join()
+                .then(function(connection){
+                    play(connection,message);
+                });
+            }      
         }
     }
     //Due to the seamless integration of Youtube in Discord, just sending the URL will give you the preview 
